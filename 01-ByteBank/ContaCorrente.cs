2c5768a1e0223e8d2cd03892601b96cf574a2588
Davi Vasconcelos <vdrdavi@gmail.com>
2021-01-11 20:23:36 -0500

    Adicionar arquivos de projeto.

---
+++ 01-ByteBank/ContaCorrente.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    public class ContaCorrente
+    {
+        public static int TotalDeContasCriadas { get; private set; }
+        public Cliente Titular { get; set; }
+        private int _agencia;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _agencia = value;
+            }
+        }
+        public int Numero { get; set; }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotalDeContasCriadas++;
+        }
+        private double _saldo;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                return true;
+            }
+        }
+        public void Depositar(double valor)
+        {
+            if (valor < 0)
+            {
+                Console.WriteLine("Valor a depositar deve ser maior que zero");
+            }
+            else
+            _saldo += valor;
+        }
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+                return false;
+            else
+            {
+                _saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(253, 525254);
+            conta.Titular = new Cliente(); 
+
+            conta.Titular.Nome = "Davi";
+            conta.Saldo = 100;
+
+            conta.Depositar(-150);
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            Console.ReadLine();
+        }
+    }
+}
