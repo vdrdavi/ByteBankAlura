845d4a58888827e14898d094be7aefeda9a00fb5
Davi Vasconcelos <vdrdavi@gmail.com>
2021-02-12 17:27:05 -0300

    Adicionando método Dividir

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -10,18 +10,20 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(253, 525254);
-            conta.Titular = new Cliente(); 
-
-            conta.Titular.Nome = "Davi";
-            conta.Saldo = 100;
-
-            conta.Depositar(-150);
-
-            Console.WriteLine(conta.Titular.Nome);
-            Console.WriteLine(conta.Saldo);
-            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            Metodo();
             Console.ReadLine();
+        }
+        static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+        public static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
         }
     }
 }
