1cfa24a1bfb2e08078e09705b95ddf4f93c4e2c7
Davi Vasconcelos <vdrdavi@gmail.com>
2021-02-18 19:51:33 -0300

    Criando construtor de exceção com argumentos

--- 01-ByteBank/SaldoInsuficienteException.cs
+++ 01-ByteBank/SaldoInsuficienteException.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace _01_ByteBank
 {
-    class SaldoInsuficienteException
+    class SaldoInsuficienteException : Exception
     {
+        public double Saldo { get; }
+        public double ValorSaque { get; }
+
+        public SaldoInsuficienteException()
+        {
+        }
+
+        public SaldoInsuficienteException(string mensagem) : base (mensagem)
+        {
+        }
+
+        public SaldoInsuficienteException(double saldo, double valorSaque) 
+            : this("Tentativa de saque de " + valorSaque + "com saldo de " + saldo)
+        {
+            Saldo = saldo;
+            ValorSaque = valorSaque;
+        }
     }
 }
