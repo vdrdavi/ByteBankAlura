185e1fe2829f5632918318af087f1155de1fd0f0
Davi Vasconcelos <vdrdavi@gmail.com>
2021-02-19 18:47:36 -0300

    Criando classe OperacaoFinanceiraException

---
+++ 01-ByteBank/OperacaoFinanceiraException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    public class OperacaoFinanceiraException : Exception
+    {
+        public OperacaoFinanceiraException()
+        {
+        }
+        public OperacaoFinanceiraException(string mensagem):base(mensagem)
+        {
+
+        }
+        public OperacaoFinanceiraException(string mensagem, Exception excecaoInterna):base(mensagem, excecaoInterna)
+        {
+
+        }
+    }
+}
