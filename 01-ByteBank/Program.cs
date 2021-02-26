21a942f6e092714b02519f2e458ebb3991bcc941
Davi Vasconcelos <vdrdavi@gmail.com>
2021-02-26 19:57:42 -0300

    Adicionando método CarregarContas e limpando código

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -10,45 +10,18 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            try
-            {
-                ContaCorrente conta1 = new ContaCorrente(1243, 12344);
-                ContaCorrente conta2 = new ContaCorrente(1243, 12344);
-                conta1.Transferir(10000, conta2);
-                //conta1.Sacar(10000);
-            }
-            catch(OperacaoFinanceiraException e)
-            {
-                Console.WriteLine(e.Message);
-                Console.WriteLine(e.StackTrace);
-
-                Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
-
-                Console.WriteLine(e.InnerException.Message);
-                Console.WriteLine(e.InnerException.StackTrace);
-            }
+            CarregarContas();
             Console.ReadLine();
         }
-        
-        //static void Metodo()
-        //{
-        //        TestaDivisao(0);
-        //}
-        //static void TestaDivisao (int divisor)
-        //{
-        //    Dividir(10, divisor);
-        //}
-        //public static int Dividir(int numero, int divisor)
-        //{
-        //    try
-        //    {
-        //        return numero / divisor;
-        //    }
-        //    catch
-        //    {
-        //        Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
-        //        throw;
-        //    }
-        //}
+        private static void CarregarContas()
+        {
+            LeitorDeArquivo leitor = new LeitorDeArquivo("contas.txt");
+
+            leitor.LerProximaLinha();
+            leitor.LerProximaLinha();
+            leitor.LerProximaLinha();
+
+            leitor.Fechar();
+        }
     }
 }
